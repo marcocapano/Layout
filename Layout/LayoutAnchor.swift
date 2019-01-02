6f63662b6c6733b0e33cee078b5bc46324fa3ebe
mrc <marco.capano@pushapp.me>
2019-01-02 23:58:59 +0100

    Added two convenience functions

--- Layout/LayoutAnchor.swift
+++ Layout/LayoutAnchor.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
+///An anchor that can be constrained to be equal to, greater than and less ther another anchor.
 public protocol LayoutAnchor {
     func constraint(equalTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
     func constraint(greaterThanOrEqualTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
     func constraint(lessThanOrEqualTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
 }
 
+///A dimension that can be constrained to be equal to, greater than and less ther another dimension or constant value.
 public protocol LayoutDimension {
     func constraint(equalToConstant: CGFloat) -> NSLayoutConstraint
     func constraint(equalTo: Self, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
--- Layout/UIView+Extension.swift
+++ Layout/UIView+Extension.swift
@@ -19,3 +19,42 @@ public extension UIView {
     }
     
 }
+
+//MARK: - Convenience functions
+public extension UIView {
+    
+    
+    /// Wraps the receiver inside the given view, with offsets.
+    /// If the receiver is not a subview of the given view, it automatically adds it.
+    ///
+    /// - Parameters:
+    ///   - view: The view to wrap into
+    ///   - edges: The insets from the "box" edges. Default value is **0**.
+    public func box(in view: UIView, edges: UIEdgeInsets = .zero) {
+        if isDescendant(of: view) == false {
+            view.addSubview(self)
+        }
+        
+        layout { proxy in
+            proxy.top == view.topAnchor + edges.top
+            proxy.bottom == view.bottomAnchor + edges.bottom
+            proxy.leading == view.leadingAnchor + edges.left
+            proxy.trailing == view.trailingAnchor + edges.right
+        }
+    }
+    
+    /// Constraints the receiver's width and height to given constant values.
+    ///
+    /// - Parameters:
+    ///   - width: The constant width value. No constraint is added when the value is nil.  Default is **nil**.
+    ///   - height: The constant height value. No constraint is added when the value is nil.  Default is **nil**.
+    public func constraintSize(width: CGFloat? = nil, height: CGFloat? = nil) {
+        if let width = width {
+            layout { proxy in  proxy.width == width }
+        }
+        
+        if let height = height {
+            layout { proxy in proxy.height == height }
+        }
+    }
+}
